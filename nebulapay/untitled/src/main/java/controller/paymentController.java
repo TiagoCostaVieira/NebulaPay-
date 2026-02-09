6421862d5bdaab26dc825828f58e7d51a4f22e11
Tiago Vieira <tiagocosvieira@gmail.com>
2026-02-09 17:07:55 -0300

    resolvendo dependencias

--- nebulapay/untitled/src/main/java/controller/paymentController.java
+++ nebulapay/untitled/src/main/java/controller/paymentController.java
@@ -1,6 +1,11 @@
 package controller;
+import com.stripe.exception.IdempotencyException;
+import dto.CreatePaymentResponse;
 
-
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.UUID;
@@ -24,8 +29,26 @@ public class paymentController{
 
         String effectiveIdempotency = idempotencyKey != null ? idempotencyKey : generateIdempotencyKey();
 
+        try{
+            createPaymentResponse response = paymentOrchestrator.createPayment(request, effectiveIdempotency);
+            log.info("Payment successfully created. PaymentId: {}", response.paymentId());
+
+            return ResponseEntity.status(HttpStatus.CREATED).body(response);
+
+        } catch (IdempotencyException e){
+            log.warn("Idempotence conflict detected. Key: {}", maskIdempotencyKey(effectiveKey));
+
+            return  ResponseEntity.status(HttpStatus.CONFLICT).body(new
+                    ErrorResponse("Payment already processed with this idempotence key."));
+
+        }
+
 
         return PaymentController.process(idempotencyKey, process);
+    }
+
+    private String generateIdempotencyKey(){
+        return UUID.randomUUID().toString();
     }
 
 }
--- nebulapay/untitled/src/main/java/dto/CreatePaymentResponse.java
+++ nebulapay/untitled/src/main/java/dto/CreatePaymentResponse.java
@@ -1,17 +1,34 @@
 package dto;
 
-
-
 import com.fasterxml.jackson.annotation.JsonInclude;
 import io.swagger.v3.oas.annotations.media.Schema;
-import lambok.Builder;
-import lambok.Data;
+import lombok.Builder;
+import lombok.Data;
 
 @Data
 @Builder
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @Schema(description = "Payment creation response")
 public class CreatePaymentResponse {
+    @Schema
+    private String paymentId;
 
+    @Schema
+    private String stripePaymentIntentId;
+
+    @Schema
+    private string status;
+
+    @Schema
+    private String customerId;
+
+    @Schema
+    private  String clientSecret;
+
+    @Schema
+    private long amount;
+
+    @Schema
+    private String currency;
 
 }
